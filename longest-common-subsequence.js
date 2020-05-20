30abbd9ef65560801c9e3db913070fcb43c88bb4
Kostiantyn Kisil <kostiantyn.kisil@gmail.com>
2020-05-21 01:48:08 +0300

    add longestCommonSubsequenceMemo

--- longest-common-subsequence.js
+++ longest-common-subsequence.js
@@ -16,5 +16,44 @@ const traverse = (seqA, curentAIndex, seqB, curentBIndex) => {
     }
 }
 
+const memoize = (fn, cache) => {
+    cache = cache || new Map();
 
-module.exports = { longestCommonSubsequence };
+    return (...args) => {
+        const hash = args.map(a => a.toString()).join('');
+
+        if (cache.has(hash)) {
+            return cache.get(hash);
+        }
+
+        const result = fn(memoize(fn, cache), ...args);
+
+        cache.set(hash, result);
+
+        return result;
+    }
+}
+
+
+const traverseWith = (fn, seqA, curentAIndex, seqB, curentBIndex) => {
+    if (seqA.length === curentAIndex || seqB.length === curentBIndex) {
+        return [];
+    } else if (seqA[curentAIndex] === seqB[curentBIndex]) {
+        return [
+            seqA[curentAIndex],
+            ...fn(seqA, curentAIndex + 1, seqB, curentBIndex + 1)
+        ];
+    } else {
+        const excludeLastOfA = fn(seqA, curentAIndex + 1, seqB, curentBIndex);
+        const excludeLastOfB = fn(seqA, curentAIndex, seqB, curentBIndex + 1);
+
+        return excludeLastOfA.length > excludeLastOfB.length ? excludeLastOfA : excludeLastOfB;
+    }
+}
+
+const traverseMemo = memoize(traverseWith);
+
+const longestCommonSubsequenceMemo = (sequenceA, sequenceB) => traverseMemo(sequenceA, 0, sequenceB, 0);
+
+
+module.exports = { longestCommonSubsequence, longestCommonSubsequenceMemo };
--- longest-common-subsequence.spec.js
+++ longest-common-subsequence.spec.js
@@ -1,5 +1,5 @@
 const {expect} = require('chai');
-const {longestCommonSubsequence} = require('./longest-common-subsequence');
+const {longestCommonSubsequence, longestCommonSubsequenceMemo} = require('./longest-common-subsequence');
 
 describe('#longestCommonSubsequnce', () => {
     it('case #1', () => {
@@ -11,7 +11,7 @@ describe('#longestCommonSubsequnce', () => {
         expect(longestCommonSubsequence(arr1, arr2)).to.deep.eq(expected);
     });
 
-    it('case #2', () => {
+    it.only('case #2', () => {
         const arr1 = ['Dog', 'Ludacious', 'Ludacious', 'Anakin', 'Dog', 'Yam'];
         const arr2 = ['Ludacious', 'Spyro', 'Anakin', 'Spyro', 'Dog', 'Plum', 'Yam'] ;
 
@@ -54,5 +54,61 @@ describe('#longestCommonSubsequnce', () => {
         const expected = ['Ginger', 'Spyro', 'Tea'];
 
         expect(longestCommonSubsequence(arr1, arr2)).to.deep.eq(expected);
+    });
+});
+
+describe('#longestCommonSubsequenceMemo', () => {
+    it('case #1', () => {
+        const arr1 = ['Dog', 'Cat'];
+        const arr2 = ['Dog', 'Bat', 'Cat'];
+
+        const expected = ['Dog', 'Cat'];
+
+        expect(longestCommonSubsequenceMemo(arr1, arr2)).to.deep.eq(expected);
+    });
+
+    it.only('case #2', () => {
+        const arr1 = ['Dog', 'Ludacious', 'Ludacious', 'Anakin', 'Dog', 'Yam'];
+        const arr2 = ['Ludacious', 'Spyro', 'Anakin', 'Spyro', 'Dog', 'Plum', 'Yam'] ;
+
+        const expected = ['Ludacious', 'Anakin', 'Dog', 'Yam'];
+
+        expect(longestCommonSubsequenceMemo(arr1, arr2)).to.deep.eq(expected);
+    });
+
+    it('case #3', () => {
+        const arr1 = ['Dog', 'Dog', 'Dog', 'Dog'];
+        const arr2 = ['Dog', 'Dog'];
+
+        const expected = ['Dog', 'Dog'];
+
+        expect(longestCommonSubsequenceMemo(arr1, arr2)).to.deep.eq(expected);
+    });
+
+    it('case #4', () => {
+        const arr1 = ['Tea', 'Tea', 'Tea'];
+        const arr2 = [];
+
+        const expected = [];
+
+        expect(longestCommonSubsequenceMemo(arr1, arr2)).to.deep.eq(expected);
+    });
+
+    it('case #5', () => {
+        const arr1 = ['Dog', 'Yam', 'Yam', 'Dog'];
+        const arr2 = ['Dog', 'Yam', 'Trout', 'Dog', 'Yam', 'Dog'];
+
+        const expected = ['Dog', 'Yam', 'Yam', 'Dog'];
+
+        expect(longestCommonSubsequenceMemo(arr1, arr2)).to.deep.eq(expected);
+    });
+
+    it('case #6', () => {
+        const arr1 = ['Ginger', 'Dog', 'Spyro', 'Yam', 'Tea'];
+        const arr2 = ['Dog', 'Yam', 'Ginger', 'Spyro', 'Tea'];
+
+        const expected = ['Ginger', 'Spyro', 'Tea'];
+
+        expect(longestCommonSubsequenceMemo(arr1, arr2)).to.deep.eq(expected);
     });
 });
